24d7db4b49e810fe9ef20baec5ec2fcbe980bd3b
yleaves <yi.leaves@foxmail.com>
2026-03-12 17:41:31 +0800

    模板

--- 01_first_demo/main.go
+++ 01_first_demo/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"html/template"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
+	r.SetFuncMap(template.FuncMap{
+		"safe": func(str string) template.HTML {
+			return template.HTML(str)
+		},
+	})
+	r.LoadHTMLGlob("./templates/**/*")
+	r.GET("/posts/index", func(c *gin.Context) {
+		c.HTML(200, "posts_index.tmpl", gin.H{
+			"message": "我是posts",
+		})
+	})
+	r.GET("/users/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "users_index.tmpl", gin.H{
+			"message": "<a href='https://www.baidu.com'>百度主页</a>",
 		})
 	})
 	r.Run()
