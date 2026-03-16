bd5557664d3a2a2eb9a6c7bf8d24245e211b9549
yleaves <yi.leaves@foxmail.com>
2026-03-16 14:50:32 +0800

    路由分组

--- 05_router/router/router.go
+++ 05_router/router/router.go
@@ -15,36 +15,14 @@ func retHelloGinAndMethod(c *gin.Context) {
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/", retHelloGinAndMethod)
-	router.POST("/", retHelloGinAndMethod)
-	router.PUT("/", retHelloGinAndMethod)
-	router.DELETE("/", retHelloGinAndMethod)
-	router.PATCH("/", retHelloGinAndMethod)
-	router.HEAD("/", retHelloGinAndMethod)
-	router.OPTIONS("/", retHelloGinAndMethod)
-
-	router.GET("/user/:name", handler.UserSave)
-	router.GET("/user", handler.UserSaveByQuery)
-	// router.GET("/", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "hello gin get method")
-	// })
-	// router.POST("/", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "hello gin post method")
-	// })
-	// router.PUT("/", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "hello gin put method")
-	// })
-	// router.DELETE("/", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "hello gin delete method")
-	// })
-	// router.PATCH("/", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "hello gin patch method")
-	// })
-	// router.HEAD("/", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "hello gin head method")
-	// })
-	// router.OPTIONS("/", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "hello gin options method")
-	// })
+	index := router.Group("/")
+	{
+		index.Any("", retHelloGinAndMethod)
+	}
+	user := router.Group("/user")
+	{
+		user.GET("/:name", handler.UserSave)
+		user.GET("", handler.UserSaveByQuery)
+	}
 	return router
 }
